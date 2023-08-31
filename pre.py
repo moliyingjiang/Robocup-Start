2b2de0713ea25f5bcf8f241f1b4a34e062d3c9aa
moliyingjiang <2996036313@qq.com>
2023-09-01 00:10:47 +0800

    修改了windows与linux的不兼容性

--- pre.py
+++ pre.py
@@ -1,5 +1,5 @@
-with open('output.txt') as f:
-    with open('last-output.txt', 'w') as out: 
+with open('output.txt', encoding = 'utf-8') as f:
+    with open('last-output.txt', 'w', encoding = 'utf-8') as out: 
         for line in f:
             if line.startswith('[') and line.endswith(']\n'):
                 out.write(line)
